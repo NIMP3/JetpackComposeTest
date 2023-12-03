08ecfa04f2cfa663d9ee3cb42f9b83e792f77d5e
Edwin Yovany <yovany.dev@gmail.com>
2023-12-02 19:12:16 -0500

    [BUILD] (Compose state): Implemented state [Remember Saveable - State property]

--- app/src/main/java/dev/yovany/jcudemy/MainActivity.kt
+++ app/src/main/java/dev/yovany/jcudemy/MainActivity.kt
@@ -3,11 +3,19 @@ package dev.yovany.jcudemy
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import dev.yovany.jcudemy.ui.theme.JCUdemyTheme
@@ -22,7 +30,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Edwin")
+                    MyStateExample()
                 }
             }
         }
@@ -30,17 +38,24 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MyStateExample() {
+    var counter by rememberSaveable {mutableStateOf(0)}
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = { counter++ }) {
+            Text(text = "Pulsame")
+        }
+        Text(text = "Has pulsado el boton ${counter} veces")
+    }
 }
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     JCUdemyTheme {
-        Greeting("Edwin")
+        MyStateExample()
     }
 }
